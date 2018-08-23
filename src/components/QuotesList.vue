8f5323752bbd6c7bb9c1138d20108e7539efd198
Maja Markovic <markovicm9000@gmail.com>
2018-08-23 16:41:40 +0200

    progress_bar_zavrsen

--- src/components/QuotesList.vue
+++ src/components/QuotesList.vue
@@ -2,8 +2,8 @@
     <div>
         <h4>Quotes added: {{ quotes.length }} of {{ maxQuotes }}</h4>
         <div class="progress">
-            <div class="progress-bar"
-            :style="{width: quotes.length * 100 / maxQuotes}" ></div>
+            <div class="progress-bar" role="progressbar" 
+            :style="{width: (quotes.length * 100 / maxQuotes) + '%' }" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
         <div id="card">
             <div class="card" style="width: 18rem;" v-for="quote in quotes" :key="quote.id">
@@ -48,6 +48,7 @@ export default {
 h4 {
     text-align: center;
 }
+
 
 </style>
 
