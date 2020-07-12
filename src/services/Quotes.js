f6bec029fb1586b56a6d345d2de43be8d99ee239
Maja Markovic <markovicm9000@gmail.com>
2020-07-12 20:27:52 +0200

    changes

--- src/services/Quotes.js
+++ src/services/Quotes.js
@@ -4,13 +4,12 @@ const quotesList = []
 class QuotesService {
     list(){
         return quotesList;
-    };
+    }
 
     addQuote(quote){
         quotesList.push(quote);
-        console.log(quotesList)
         
-    };
+    }
 
     delete(quote){
         quotesList.splice(quotesList.indexOf(quote), 1)
